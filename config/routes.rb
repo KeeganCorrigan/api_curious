a238b0e9928a22b2eba3c03a50b9ab53726a2b29
KeeganCorrigan <keegan.rw.corrigan@gmail.com>
2018-08-23 11:37:59 -0600

    100% test coverage

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   get "/auth/:provider/callback", to: "sessions#create"
   get 'auth/failure', to: redirect('/')
-  delete 'signout', to: 'sessions#destroy', as: 'signout'
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
   root to: 'sessions#new'
 
   resources :users, only: [:show] do
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,26 +3,23 @@ require 'rails_helper'
 describe "a user visits /" do
   describe "clicks on login" do
     it "visits the home page" do
+
       stub_omniauth
+
+      json_response_1 = File.open("./fixtures/github_starred.json")
+      stub_request(:get, "https://api.github.com/users/KeeganCorrigan/starred").to_return(status: 200, body: json_response_1)
+
+      date = 1.week.ago.strftime("%Y-%m-%d")
+
+      json_response_2 = File.open("./fixtures/github_recent_commits.json")
+      stub_request(:get, "https://api.github.com/search/commits?q=committer-name:KeeganCorrigan+committer-date:>#{date}").to_return(status: 200, body: json_response_2)
 
       visit "/"
 
       click_on "Sign In with Github"
 
-      # TODO: redirect to dashboard path
-
       expect(current_path).to eq(user_path(User.last))
       expect(page).to have_content("Logout")
-    end
-  end
-
-  describe "clicks on logout" do
-    xit "goes back to root" do
-      user = create(:user)
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      visit user_path(user)
 
       click_on "Logout"
 
